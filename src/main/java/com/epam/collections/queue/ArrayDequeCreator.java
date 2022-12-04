59e7c32d39360be59b5a74183bfadc98ed2ec9fb
Diana Veltauri <diana.veltauri1@gmail.com>
2022-12-05 00:24:42 +0400

    Init commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,33 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque <Integer> res = new ArrayDeque<>();
+
+        moveCards(firstQueue,res);
+        moveCards(secondQueue,res);
+
+        while (true){
+            if(firstQueue.size() == 0) break;
+            firstQueue.add(res.removeLast());
+            moveCards(firstQueue,res);
+            secondQueue.add(res.removeLast());
+            moveCards(secondQueue,res);
+        }
+        return res;
+    }
+
+    private void moveCards (Queue<Integer> from, ArrayDeque<Integer> to){
+        if(from.size()>=2) {
+            to.addLast(from.remove());
+            to.addLast(from.remove());
+        }else {
+            to.addLast(from.remove());
+        }
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List <Integer> res = new ArrayList<>();
+        List <Integer> dishes = new ArrayList<>();
+
+        for (int i=1; i<=numberOfDishes; i++){
+            dishes.add(i);
+        }
+
+        int i=everyDishNumberToEat-1;
+        while (dishes.size()>0){
+            if(i>=dishes.size()) i=i%dishes.size();
+            res.add(dishes.get(i));
+            dishes.remove(i);
+            i--;
+            i+=everyDishNumberToEat;
+        }
+        return res;
     }
 }
